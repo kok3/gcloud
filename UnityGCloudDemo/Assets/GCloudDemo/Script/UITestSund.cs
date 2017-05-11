12daf9c60eaa639fcd799e61985dcc44321727d1
kok3 <kok3@users.noreply.github.com>
2017-05-11 16:55:47 +0800

    task:00 ios封装

--- UnityGCloudDemo/Assets/GCloudDemo/Script/UITestSund.cs
+++ UnityGCloudDemo/Assets/GCloudDemo/Script/UITestSund.cs
@@ -1,9 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Runtime.InteropServices;
 
-using GiantSDK.Voice;
 public class UITestSund : MonoBehaviour {
+
+	const string SDK_DLL = "__Internal";
+
+	#if UNITY_IOS && !UNITY_EDITOR
+
+		[DllImport(SDK_DLL)]
+
+		public static extern void cStartSpeaker();
+
+		[DllImport(SDK_DLL)]
+
+		public static extern void cStopSpeaker();
+
+		[DllImport(SDK_DLL)]
+
+		public static extern void cStartMic();
+
+		[DllImport(SDK_DLL)]
+
+		public static extern void cStopMic();
+
+		[DllImport(SDK_DLL)]
+
+		public static extern void cEnterRoom();
+
+		[DllImport(SDK_DLL)]
+
+		public static extern void cQuitRoom();
+		 
+	#endif
 
     private static AndroidJavaClass androidJavaClass = null;
     protected AndroidJavaObject gCloudJavaObject = null;
@@ -46,9 +76,13 @@ public class UITestSund : MonoBehaviour {
         //Debug.LogError("@@@onEnterRoom");
 
         setTip("onEnterRoom");
-
+	
+		#if UNITY_ANDROID && !UNITY_EDITOR
         if (gCloudJavaObject != null)
             gCloudJavaObject.Call("stopRecord");
+		#elif UNITY_IOS && !UNITY_EDITOR
+			cEnterRoom();
+		#endif
 
     }
     //退出房间
@@ -57,38 +91,61 @@ public class UITestSund : MonoBehaviour {
         Debug.LogError("@@@onQuitRoom");
 
         setTip("onQuitRoom");
+
+		#if UNITY_ANDROID && !UNITY_EDITOR
+
+		#elif UNITY_IOS && !UNITY_EDITOR
+			cQuitRoom();
+		#endif
+
     }
     //开启麦克风
     public void onStartMic()
     {
         //Debug.Log("@@@onStartMic");
-        if (gCloudJavaObject != null)
-            gCloudJavaObject.Call("startRecord");
+		#if UNITY_ANDROID && !UNITY_EDITOR
+	        if (gCloudJavaObject != null)
+	            gCloudJavaObject.Call("startRecord");
+		#elif UNITY_IOS && !UNITY_EDITOR
+			cStartMic();
+		#endif
     }
     //关闭麦克风
     public void onStopMic()
     {
         //Debug.Log("@@@onStopMic");
-        if (gCloudJavaObject != null)
-        {
-            Debug.Log("@@@onStopMic");
-            gCloudJavaObject.Call("stopRecord");
-        }
+		#if UNITY_ANDROID && !UNITY_EDITOR
+	        if (gCloudJavaObject != null)
+	        {
+	            Debug.Log("@@@onStopMic");
+	            gCloudJavaObject.Call("stopRecord");
+	        }
+		#elif UNITY_IOS && !UNITY_EDITOR
+			cStopMic();
+		#endif
     }
     //开启扬声器
     public void onStartSpeaker()
     {
         //Debug.Log("@@@onStartSpeaker");
-        if (gCloudJavaObject != null)
-            gCloudJavaObject.Call("startListen");
+		#if UNITY_ANDROID && !UNITY_EDITOR
+	        if (gCloudJavaObject != null)
+	            gCloudJavaObject.Call("startListen");
+		#elif UNITY_IOS && !UNITY_EDITOR
+			cStartSpeaker();
+		#endif
     }
     //关闭扬声器
     public void onStopSpeaker()
     {
         //Debug.Log("@@@onStopSpeaker");
         //Debug.LogError("@@@onStartSpeaker");
-        if (gCloudJavaObject != null)
-            gCloudJavaObject.Call("stopListen");
+		#if UNITY_ANDROID && !UNITY_EDITOR
+	        if (gCloudJavaObject != null)
+	            gCloudJavaObject.Call("stopListen");
+		#elif UNITY_IOS && !UNITY_EDITOR
+			cStopSpeaker();
+		#endif
     }
 
 
